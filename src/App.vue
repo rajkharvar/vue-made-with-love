4c3ddcf81af2beec7c46e271c1e7ae797eee5cb2
rajkharvar <rajkharvar1808@gmail.com>
2020-03-04 20:22:26 +0530

    lazy commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MadeWithLove by="Raj Kharvar" link="https://github.com" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MadeWithLove from './components/MadeWithLove.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MadeWithLove
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MadeWithLove.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="made-with-love">
+    <p>
+      {{ code ? '&lt;/&gt;' : 'Made' }} with ❤️
+      {{ by ? 'by' : '' }}
+      <span v-if="by" class="by">
+        <a :href="link" v-if="link">{{ by }}</a>
+        {{ !link ? by : '' }}
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MadeWithLove',
+  props: {
+    by: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: ''
+    },
+    code: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
